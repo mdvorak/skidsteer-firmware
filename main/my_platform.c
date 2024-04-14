d4fc63545cd2b6e608640faa5ebf4be0bf9d6d04
Michal Dvořák <mikee2185@gmail.com>
2024-04-14 13:06:04 +0200

    chore: bare init

--- main/my_platform.c
+++ main/my_platform.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <uni.h>
+#include <parser/uni_hid_parser_wii.h>
 
 // Custom "instance"
 typedef struct my_platform_instance_s {
@@ -7,135 +8,67 @@ typedef struct my_platform_instance_s {
 } my_platform_instance_t;
 
 // Declarations
-static void trigger_event_on_gamepad(uni_hid_device_t* d);
-static my_platform_instance_t* get_my_platform_instance(uni_hid_device_t* d);
+static void trigger_event_on_gamepad(uni_hid_device_t *d);
+
+static my_platform_instance_t *get_my_platform_instance(uni_hid_device_t *d);
 
 //
 // Platform Overrides
 //
-static void my_platform_init(int argc, const char** argv) {
+static void my_platform_init(int argc, const char **argv) {
     ARG_UNUSED(argc);
     ARG_UNUSED(argv);
 
+    // Configure WiiMote
+    wii_force_flags(WII_FLAGS_NATIVE);
+
     logi("custom: init()\n");
-
-#if 0
-    uni_gamepad_mappings_t mappings = GAMEPAD_DEFAULT_MAPPINGS;
-
-    // Inverted axis with inverted Y in RY.
-    mappings.axis_x = UNI_GAMEPAD_MAPPINGS_AXIS_RX;
-    mappings.axis_y = UNI_GAMEPAD_MAPPINGS_AXIS_RY;
-    mappings.axis_ry_inverted = true;
-    mappings.axis_rx = UNI_GAMEPAD_MAPPINGS_AXIS_X;
-    mappings.axis_ry = UNI_GAMEPAD_MAPPINGS_AXIS_Y;
-
-    // Invert A & B
-    mappings.button_a = UNI_GAMEPAD_MAPPINGS_BUTTON_B;
-    mappings.button_b = UNI_GAMEPAD_MAPPINGS_BUTTON_A;
-
-    uni_gamepad_set_mappings(&mappings);
-#endif
-    //    uni_bt_service_set_enabled(true);
 }
 
 static void my_platform_on_init_complete(void) {
     logi("custom: on_init_complete()\n");
 
-    // Safe to call "unsafe" functions since they are called from BT thread
-
-    // Start scanning
+    // Listen
     uni_bt_enable_new_connections_unsafe(true);
-
-    // Based on runtime condition, you can delete or list the stored BT keys.
-    if (1)
-        uni_bt_del_keys_unsafe();
-    else
-        uni_bt_list_keys_unsafe();
 }
 
-static void my_platform_on_device_connected(uni_hid_device_t* d) {
+static void my_platform_on_device_connected(uni_hid_device_t *d) {
     logi("custom: device connected: %p\n", d);
+    // Only one controller allowed
+    uni_bt_enable_new_connections_unsafe(false);
 }
 
-static void my_platform_on_device_disconnected(uni_hid_device_t* d) {
+static void my_platform_on_device_disconnected(uni_hid_device_t *d) {
     logi("custom: device disconnected: %p\n", d);
+    uni_bt_enable_new_connections_unsafe(true);
 }
 
-static uni_error_t my_platform_on_device_ready(uni_hid_device_t* d) {
+static uni_error_t my_platform_on_device_ready(uni_hid_device_t *d) {
     logi("custom: device ready: %p\n", d);
-    my_platform_instance_t* ins = get_my_platform_instance(d);
+    my_platform_instance_t *ins = get_my_platform_instance(d);
     ins->gamepad_seat = GAMEPAD_SEAT_A;
 
     trigger_event_on_gamepad(d);
     return UNI_ERROR_SUCCESS;
 }
 
-static void my_platform_on_controller_data(uni_hid_device_t* d, uni_controller_t* ctl) {
-    static uint8_t leds = 0;
-    static uint8_t enabled = true;
-    static uni_controller_t prev = {0};
-    uni_gamepad_t* gp;
+static void my_platform_on_gamepad(const uni_gamepad_t *gp) {
 
-    // Optimization to avoid processing the previous data so that the console
-    // does not get spammed with a lot of logs, but remove it from your project.
-    if (memcmp(&prev, ctl, sizeof(*ctl)) == 0) {
-        return;
-    }
-    prev = *ctl;
-    // Print device Id before dumping gamepad.
-    // This could be very CPU intensive and might crash the ESP32.
-    // Remove these 2 lines in production code.
-    //    logi("(%p), id=%d, \n", d, uni_hid_device_get_idx_for_instance(d));
-    //    uni_controller_dump(ctl);
+}
 
+static void my_platform_on_controller_data(uni_hid_device_t *d, uni_controller_t *ctl) {
     switch (ctl->klass) {
         case UNI_CONTROLLER_CLASS_GAMEPAD:
-            gp = &ctl->gamepad;
-
-            // Debugging
-            // Axis ry: control rumble
-            if ((gp->buttons & BUTTON_A) && d->report_parser.play_dual_rumble != NULL) {
-                d->report_parser.play_dual_rumble(d, 0 /* delayed start ms */, 250 /* duration ms */,
-                                                  255 /* weak magnitude */, 0 /* strong magnitude */);
-            }
-            // Buttons: Control LEDs On/Off
-            if ((gp->buttons & BUTTON_B) && d->report_parser.set_player_leds != NULL) {
-                d->report_parser.set_player_leds(d, leds++ & 0x0f);
-            }
-            // Axis: control RGB color
-            if ((gp->buttons & BUTTON_X) && d->report_parser.set_lightbar_color != NULL) {
-                uint8_t r = (gp->axis_x * 256) / 512;
-                uint8_t g = (gp->axis_y * 256) / 512;
-                uint8_t b = (gp->axis_rx * 256) / 512;
-                d->report_parser.set_lightbar_color(d, r, g, b);
-            }
-
-            // Toggle Bluetooth connections
-            if ((gp->buttons & BUTTON_SHOULDER_L) && enabled) {
-                logi("*** Disabling Bluetooth connections\n");
-                uni_bt_enable_new_connections_safe(false);
-                enabled = false;
-            }
-            if ((gp->buttons & BUTTON_SHOULDER_R) && !enabled) {
-                logi("*** Enabling Bluetooth connections\n");
-                uni_bt_enable_new_connections_safe(true);
-                enabled = true;
-            }
-            break;
+            my_platform_on_gamepad(&ctl->gamepad);
         default:
             break;
     }
 }
 
-static const uni_property_t* my_platform_get_property(uni_property_idx_t idx) {
-    ARG_UNUSED(idx);
-    return NULL;
-}
-
-static void my_platform_on_oob_event(uni_platform_oob_event_t event, void* data) {
+static void my_platform_on_oob_event(uni_platform_oob_event_t event, void *data) {
     switch (event) {
         case UNI_PLATFORM_OOB_GAMEPAD_SYSTEM_BUTTON: {
-            uni_hid_device_t* d = data;
+            uni_hid_device_t *d = data;
 
             if (d == NULL) {
                 loge("ERROR: my_platform_on_oob_event: Invalid NULL device\n");
@@ -143,7 +76,7 @@ static void my_platform_on_oob_event(uni_platform_oob_event_t event, void* data)
             }
             logi("custom: on_device_oob_event(): %d\n", event);
 
-            my_platform_instance_t* ins = get_my_platform_instance(d);
+            my_platform_instance_t *ins = get_my_platform_instance(d);
             ins->gamepad_seat = ins->gamepad_seat == GAMEPAD_SEAT_A ? GAMEPAD_SEAT_B : GAMEPAD_SEAT_A;
 
             trigger_event_on_gamepad(d);
@@ -151,7 +84,7 @@ static void my_platform_on_oob_event(uni_platform_oob_event_t event, void* data)
         }
 
         case UNI_PLATFORM_OOB_BLUETOOTH_ENABLED:
-            logi("custom: Bluetooth enabled: %d\n", (bool)(data));
+            logi("custom: Bluetooth enabled: %d\n", (bool) (data));
             break;
 
         default:
@@ -163,12 +96,12 @@ static void my_platform_on_oob_event(uni_platform_oob_event_t event, void* data)
 //
 // Helpers
 //
-static my_platform_instance_t* get_my_platform_instance(uni_hid_device_t* d) {
-    return (my_platform_instance_t*)&d->platform_data[0];
+static my_platform_instance_t *get_my_platform_instance(uni_hid_device_t *d) {
+    return (my_platform_instance_t *) &d->platform_data[0];
 }
 
-static void trigger_event_on_gamepad(uni_hid_device_t* d) {
-    my_platform_instance_t* ins = get_my_platform_instance(d);
+static void trigger_event_on_gamepad(uni_hid_device_t *d) {
+    my_platform_instance_t *ins = get_my_platform_instance(d);
 
     if (d->report_parser.play_dual_rumble != NULL) {
         d->report_parser.play_dual_rumble(d, 0 /* delayed start ms */, 150 /* duration ms */, 128 /* weak magnitude */,
@@ -190,17 +123,17 @@ static void trigger_event_on_gamepad(uni_hid_device_t* d) {
 //
 // Entry Point
 //
-struct uni_platform* get_my_platform(void) {
+struct uni_platform *get_my_platform(void) {
     static struct uni_platform plat = {
-        .name = "custom",
-        .init = my_platform_init,
-        .on_init_complete = my_platform_on_init_complete,
-        .on_device_connected = my_platform_on_device_connected,
-        .on_device_disconnected = my_platform_on_device_disconnected,
-        .on_device_ready = my_platform_on_device_ready,
-        .on_oob_event = my_platform_on_oob_event,
-        .on_controller_data = my_platform_on_controller_data,
-        .get_property = my_platform_get_property,
+            .name = "custom",
+            .init = my_platform_init,
+            .on_init_complete = my_platform_on_init_complete,
+            .on_device_connected = my_platform_on_device_connected,
+            .on_device_disconnected = my_platform_on_device_disconnected,
+            .on_device_ready = my_platform_on_device_ready,
+            .on_oob_event = my_platform_on_oob_event,
+            .on_controller_data = my_platform_on_controller_data,
+            .get_property = NULL,
     };
 
     return &plat;
